ddfb5b8b30958679ed5a1400760f7f3862e8edfa
Tony Hunter <thunter1987@yahoo.com>
2023-09-09 14:42:31 -0500

    passes tests

--- server/app.py
+++ server/app.py
@@ -4,5 +4,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    count = f''
+    for c in range(parameter):
+        count += f'{c}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == "+":
+        return str(num1 + num2)
+    elif operation == "-":
+        return str(num1 - num2)
+    elif operation == "*":
+        return str(num1 * num2)
+    elif operation == "div":
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+        
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
